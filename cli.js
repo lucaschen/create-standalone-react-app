fe595c844ed865a69103168fc69d0ad05e6d3d16
Lucas Chen <2109121+parkroolucas@users.noreply.github.com>
2018-02-07 15:53:47 +1100

    Fixed process directory issues, added basic testing environment

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ const child_process = require("child_process");
 const fs = require("fs-extra");
 const path = require("path");
 
-const pwdify = relativePath => path.resolve(__dirname, relativePath);
+const pwdify = relativePath => path.resolve(process.cwd(), relativePath);
 
 // just take first two args off for now ("node" and "cli.js")
 const argv = process.argv.slice(2);
@@ -12,11 +12,9 @@ const argv = process.argv.slice(2);
 const directoryName = argv[0];
 
 if (fs.existsSync(pwdify(directoryName))) {
-  throw new Error(`${directoryName} already exists!`);
+  console.log(`${directoryName} already exists!`);
   process.exit();
 }
-
-console.log(`Cloning into ${directoryName}...`);
 
 child_process.execSync(`git clone git@github.com:parkroolucas/react-app-starter ${directoryName}`);
 
