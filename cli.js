205ddb78d72b948c32a471ca57676c32f1a4597b
Lucas Chen <2109121+parkroolucas@users.noreply.github.com>
2018-02-07 15:54:51 +1100

    Renamed pwdify to cwdify

--- cli.js
+++ cli.js
@@ -4,21 +4,21 @@ const child_process = require("child_process");
 const fs = require("fs-extra");
 const path = require("path");
 
-const pwdify = relativePath => path.resolve(process.cwd(), relativePath);
+const cwdify = relativePath => path.resolve(process.cwd(), relativePath);
 
 // just take first two args off for now ("node" and "cli.js")
 const argv = process.argv.slice(2);
 
 const directoryName = argv[0];
 
-if (fs.existsSync(pwdify(directoryName))) {
+if (fs.existsSync(cwdify(directoryName))) {
   console.log(`${directoryName} already exists!`);
   process.exit();
 }
 
 child_process.execSync(`git clone git@github.com:parkroolucas/react-app-starter ${directoryName}`);
 
-if (!fs.existsSync(pwdify(directoryName))) {
+if (!fs.existsSync(cwdify(directoryName))) {
   console.log("Something went wrong...");
   process.exit();
 }
@@ -27,16 +27,16 @@ console.log(`Clone successful! Cleaning up...`);
 
 (async () => {
   await Promise.all([
-    fs.remove(pwdify(`${directoryName}/.git`)),
-    fs.remove(pwdify(`${directoryName}/package-lock.json`)),
+    fs.remove(cwdify(`${directoryName}/.git`)),
+    fs.remove(cwdify(`${directoryName}/package-lock.json`)),
     (async () => {
-      const packageJson = await fs.readJson(pwdify(`${directoryName}/package.json`));
+      const packageJson = await fs.readJson(cwdify(`${directoryName}/package.json`));
       packageJson.name = directoryName;
       packageJson.description = "";
       packageJson.repository.url = `git+https://github.com/???/${directoryName}.git`;
       packageJson.author = "";
 
-      await fs.writeJson(pwdify(`${directoryName}/package.json`), packageJson, {
+      await fs.writeJson(cwdify(`${directoryName}/package.json`), packageJson, {
         spaces: 2
       });
     })()
